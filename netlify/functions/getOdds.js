af2a57ad04bc038f828a8c8ad4b0d0ef35a46b42
vividsid94 <siddharth.murali2008@gmail.com>
2025-03-13 13:53:07 -0500

    Update getOdds.js

--- netlify/functions/getOdds.js
+++ netlify/functions/getOdds.js
@@ -4,7 +4,6 @@ export const handler = async () => {
   try {
     const apiKey = process.env.VITE_API_KEY; // Keep this secret in Netlify's environment variables
     const apiUrl = `https://api.api-tennis.com/tennis/?method=get_live_odds&APIkey=${apiKey}`;
-    console.log(apiKey)
     const response = await axios.get(apiUrl);
     return {
       statusCode: 200,
